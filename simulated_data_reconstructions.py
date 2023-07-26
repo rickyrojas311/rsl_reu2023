bf5b47b92a8add24352deb510b79c4ecedd1f9d2
Ricky Rojas <rickyrojas.com>
2023-07-26 14:45:19 -0700

    Small changes

--- simulated_data_reconstructions.py
+++ simulated_data_reconstructions.py
@@ -14,32 +14,33 @@ import matplotlib.pyplot as plt
 import anisotropic_class as anic
 from anisotropic_operator import normalize_matrix
 
-def display_DMI_res():
+if __name__ == "__main__":
+# def display_DMI_res():
     """
     Creates matplotlib visualization for reconstructions for different inital
     DMI resolutions
     """
     #Ground Truths
-    glx_header = nib.as_closest_canonical(nib.load(r""))
+    glx_header = nib.as_closest_canonical(nib.load(r"project_data/BraTS_Data/DMI_Simulations/DMI/patient_9/DMI_patient_9_ds_2_gm_3.0_wm_1.0_tumor_0.5_ed_2.0_noise_0_seed_1234/dmi_gt.nii.gz"))
     glx_gt = glx_header.get_fdata()[:, :, :, 0]
     glx_gt = xp.array(normalize_matrix(glx_gt))
-    lac_header = nib.as_closest_canonical(nib.load(r""))
+    lac_header = nib.as_closest_canonical(nib.load(r"project_data/BraTS_Data/DMI_Simulations/DMI/patient_9/DMI_patient_9_ds_2_gm_0.3_wm_0.1_tumor_3.0_ed_1.0_noise_0_seed_1234/dmi_gt.nii.gz"))
     lac_gt = lac_header.get_fdata()[:, :, :, 0]
     lac_gt = xp.array(normalize_matrix(lac_gt))
 
     #Structural Data
-    structural_header = nib.as_closest_canonical(nib.load(r"project_data/BraTS_Data/MRI_2mm/t1.nii.gz"))
+    structural_header = nib.as_closest_canonical(nib.load(r"project_data/BraTS_Data/DMI_Simulations/DMI/patient_9/t1.nii.gz"))
     structural_data = structural_header.get_fdata()
     structural_data = xp.array(normalize_matrix(structural_data))
 
     #Low Res Data
-    mm4_header = nib.as_closest_canonical(nib.load(r""))
+    mm4_header = nib.as_closest_canonical(nib.load(r"project_data/BraTS_Data/DMI_Simulations/DMI/patient_9/DMI_patient_9_ds_2_gm_3.0_wm_1.0_tumor_0.5_ed_2.0_noise_0_seed_1234/dmi.nii.gz"))
     data_4mm = mm4_header.get_fdata()[:, :, :, 0][::2, ::2, ::2]
     data_4mm = xp.array(normalize_matrix(data_4mm))
-    mm12_header = nib.as_closest_canonical(nib.load(r""))
+    mm12_header = nib.as_closest_canonical(nib.load(r"project_data/BraTS_Data/DMI_Simulations/DMI/patient_9/DMI_patient_9_ds_6_gm_3.0_wm_1.0_tumor_0.5_ed_2.0_noise_0_seed_1234/dmi.nii.gz"))
     data_12mm = mm12_header.get_fdata()[:, :, :, 0][::6, ::6, ::6]
     data_12mm = xp.array(normalize_matrix(data_12mm))
-    mm24_header = nib.as_closest_canonical(nib.load(r""))
+    mm24_header = nib.as_closest_canonical(nib.load(r"project_data/BraTS_Data/DMI_Simulations/DMI/patient_9/DMI_patient_9_ds_12_gm_3.0_wm_1.0_tumor_0.5_ed_2.0_noise_0_seed_1234/dmi.nii.gz"))
     data_24mm = mm24_header.get_fdata()[:, :, :, 0][::12, ::12, ::12]
     data_24mm = xp.array(normalize_matrix(data_24mm))
 
@@ -48,20 +49,20 @@ def display_DMI_res():
     oper = anic.AnatomicReconstructor(structural_data, 1e-2, 7e-3, 20000, True, save_options)
 
     #Reconstructions
-    oper.img_name = ""
+    oper.img_name = "DMI9_Glx_4mm"
     recon_4mm = oper(data_4mm)
 
-    oper.img_name = ""
+    oper.img_name = "DMI9_Glx_12mm"
     oper.given_lambda = 6e-3
     oper.given_eta = 4e-4
     recon_12mm = oper(data_12mm)
 
-    oper.img_name = ""
+    oper.img_name = "DMI9_Glx_24mm"
     oper.given_lambda = 6e-3
     oper.given_eta = 1e-3
     recon_24mm = oper(data_24mm)
 
-    SLICE= 56
+    SLICE = 66
 
     if xp.__name__ == "cupy":
             glx_gt = glx_gt.get()[:,:, SLICE]
@@ -75,16 +76,16 @@ def display_DMI_res():
     fig, ax = plt.subplots(nrows=2, ncols=4, figsize=(20,15))
     ax = ax.ravel()
     ax[0].imshow(structural_data, vmin = 0, vmax = glx_gt.max(), cmap = 'Greys_r')
-    ax[0].set_ylabel("Glx Orig")
+    ax[0].set_xlabel("Glx Orig")
     ax[0].set_title("GT")
     ax[0].axis("off")
-    ax[1].imshow(data_4mm[:, :, SLICE//2], vmin = 0, vmax = glx_gt.max(), cmap = 'Greys_r')
+    ax[1].imshow(data_4mm[:, :, SLICE//2].get(), vmin = 0, vmax = glx_gt.max(), cmap = 'Greys_r')
     ax[1].set_title("4mm")
     ax[1].axis("off")
-    ax[2].imshow(data_12mm[:, :, SLICE//6], vmin = 0, vmax = glx_gt.max(), cmap = 'Greys_r')
+    ax[2].imshow(data_12mm[:, :, SLICE//6].get(), vmin = 0, vmax = glx_gt.max(), cmap = 'Greys_r')
     ax[2].set_title("12mm")
     ax[2].axis("off")
-    ax[3].imshow(data_24mm[:, :, SLICE//12], vmin = 0, vmax = glx_gt.max(), cmap = 'Greys_r')
+    ax[3].imshow(data_24mm[:, :, SLICE//12].get(), vmin = 0, vmax = glx_gt.max(), cmap = 'Greys_r')
     ax[3].set_title("24mm")
     ax[3].axis("off")
     ax[4].imshow(glx_gt, vmin = 0, vmax = glx_gt.max(), cmap = 'Greys_r')
@@ -99,5 +100,5 @@ def display_DMI_res():
     fig.tight_layout()
     fig.show()
 
-if __name__ == "__main__":
-    display_DMI_res()
+# if __name__ == "__main__":
+#     display_DMI_res()
